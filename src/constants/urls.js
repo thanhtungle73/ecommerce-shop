a207f5f6a701fa6e91f6a8981fb80d0de27e7663
Tung Le <thanhtungle73@gmail.com>
2022-03-14 02:22:28 +0700

    Add new home banner img

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,2 +1,2 @@
 export const HOME_BANNER_IMG_URL =
-  'https://res.cloudinary.com/thanhtungle/image/upload/v1647151792/Ecommerce-app/Homepage/Banner_qbx4z2.png';
+  'https://res.cloudinary.com/thanhtungle/image/upload/v1647199037/Ecommerce-app/Homepage/Homepage_banner_tmuzwd.png';
